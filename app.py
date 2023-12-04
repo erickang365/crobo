967d6ce93f02dff857fc9b125f84248d14802be7
erickang365 <72977525+erickang365@users.noreply.github.com>
2023-12-04 21:33:47 +0900

    초기메세지 출력되게 코드 수정

--- app.py
+++ app.py
@@ -19,44 +19,40 @@ thread_id = st.session_state.thread_id
 #미리 만들어 둔 Assistant
 assistant_id = "asst_8jhwuoPJibwLhyBApNgHYyvf"
 
-#메세지 모두 불러오기
-thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
-
-#페이지 제목
-# st.header("Kcosw.com AI helper")
-
+# Streamlit 페이지 제목
 st.title("💬 Kcosw.com Chatbot")
 st.caption("🚀 A AI chatbot powered by CROBO Corp.")
+
+# 초기 메시지 설정
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Do you want to buy Korean cosmetics wholesale?"}]
 
+# 메시지를 화면에 표시
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-#메세지 역순으로 가져와서 UI에 뿌려주기
-for msg in thread_messages.data:
-    with st.chat_message(msg.role):
-        st.write(msg.content[0].text.value)
-
-#입력창에 입력을 받아서 입력된 내용으로 메세지 생성
+# 사용자 입력 받기
 prompt = st.chat_input("Enter what you want to ask!")
 if prompt:
+    # 사용자의 메시지를 session_state에 추가
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+
+    # OpenAI API를 통해 메시지 생성
     message = client.beta.threads.messages.create(
         thread_id=thread_id,
         role="user",
         content=prompt
     )
 
-    #입력한 메세지 UI에 표시
-    with st.chat_message(message.role):
-        st.write(message.content[0].text.value)
-
-    #RUN을 돌리는 과정
+    # RUN을 돌리는 과정
     run = client.beta.threads.runs.create(
         thread_id=thread_id,
         assistant_id=assistant_id,
     )
 
     with st.spinner('Waiting for response...'):
-        #RUN이 completed 되었나 1초마다 체크
+        # RUN이 completed 되었나 체크
         while run.status != "completed":
             time.sleep(1)
             run = client.beta.threads.runs.retrieve(
@@ -64,10 +60,10 @@ if prompt:
                 run_id=run.id
             )
 
-    #while문을 빠져나왔다는 것은 완료됐다는 것이니 메세지 불러오기
+    # 완료되었으니 메시지 불러오기
     messages = client.beta.threads.messages.list(
         thread_id=thread_id
     )
-    #마지막 메세지 UI에 추가하기
-    with st.chat_message(messages.data[0].role):
-        st.write(messages.data[0].content[0].text.value)
+    # 마지막 메시지 UI에 추가
+    response_content = messages.data[0].content[0].text.value
+    st.session_state["messages"].append({"role": "assistant", "content": response_content})
